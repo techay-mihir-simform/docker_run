ba8b6922916767decd315e3ebd4ebd5c1e82d553
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-11-27 15:33:58 +0530

    Update remove_backend_node.sh

--- scripts/remove_backend_node.sh
+++ scripts/remove_backend_node.sh
@@ -1,10 +1,11 @@
 #!/bin/bash  
 
 # #docker-compose
-cd /home/ubuntu/docker_run
+
 if [ -d /home/ubuntu/docker_run ];then
+    cd /home/ubuntu/docker_run
     docker-compose down 
-    docker rmi 853075046379.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+    docker rmi -f 853075046379.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
     rm -rf /home/ubuntu/docker_run
 fi 
 
